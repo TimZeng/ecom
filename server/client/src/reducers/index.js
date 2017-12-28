10e40aec8f52e1323091ba568ddb90806fbc82ee
Tim Zeng <zengtiantian1122@gmail.com>
2017-12-28 13:58:13 -0800

    setup survey form with redux-form

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
-
+import { reducer as formReducer } from 'redux-form';
 import AuthReducer from './authReducer';
 
 const rootReducer = combineReducers({
-  auth: AuthReducer
+  auth: AuthReducer,
+  form: formReducer
 });
 
 export default rootReducer;
